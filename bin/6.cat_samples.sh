95d60a6e94567f904c60d26ff9fa0a7be38ea9cb
Cris <isaurarosasr@gmail.com>
2020-11-06 11:08:02 -0600

    comentado

--- bin/6.cat_samples.sh
+++ bin/6.cat_samples.sh
@@ -16,18 +16,18 @@ mkdir -p ../out/DE_cat
 
 mkdir -p ../out/DS_cat
 
-#cat samples
+#concatenate DE_fasta and DS_fasta samples
 
 for i in DE DS; do
 for k in `ls ../data/${i}_fasta/ | grep ".fasta" | sed "s/_1P.fasta//"| sed "s/_2P.fasta//" | uniq` ; do
 
 echo ${k}
 
-cat ../data/${i}_fasta/${k}_1P.fasta > ${i}_cat_1.fa
+cat ../data/${i}_fasta/${k}_1P.fasta > ${i}_cat_1.fa  #cat reverse samples
 
-cat ../data/${i}_fasta/${k}_2P.fasta > ${i}_cat_2.fa
+cat ../data/${i}_fasta/${k}_2P.fasta > ${i}_cat_2.fa  #cat forward samples
 
-mv ${i}_cat_1.fa ${i}_cat_2.fa ../out/${i}_cat/
+mv ${i}_cat_1.fa ${i}_cat_2.fa ../out/${i}_cat/  #mv samples to out_file
 
 done
  done
