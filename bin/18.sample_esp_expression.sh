45ff1d0ce6b904cd8d330081fad2ac8208eb99a7
Isaura Rosas Reinhold <35971939+IsauraRReinhold@users.noreply.github.com>
2021-01-06 16:54:41 -0600

    Delete 18.sample_esp_expression.sh
    this script is wrong

--- bin/18.sample_esp_expression.sh
+++
@@ -1,8 +0,0 @@
-
-
-
-
-/home/cris/Documentos/trinityrnaseq/Analysis/DifferentialExpression/replicates_to_sample_averages_matrix.pl \
-    --matrix ../Trinity_trans.TMM.EXPR.matrix \
-    --samples_file ../samples.txt \
-    --avg_log_val
